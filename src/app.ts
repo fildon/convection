a0fc796026d2fa1f1baaf0057a486c2499976c74
Rupert Foggo McKay <rumckay@expedia.com>
2020-02-28 11:10:29 +0000

    Adds front end; currently just drawing a red circle

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log("foo");
+document.addEventListener("DOMContentLoaded", () => {
+    const canvasElement = document.getElementById("canvas") as HTMLCanvasElement;
+    const ctx = canvasElement.getContext("2d")!;
+    canvasElement.width = window.innerWidth;
+    canvasElement.height = window.innerHeight;
+    ctx.beginPath();
+    ctx.arc(
+        window.innerWidth / 2,
+        window.innerHeight / 2,
+        20,
+        0, 2 * Math.PI
+    );
+    ctx.fillStyle = 'red';
+    ctx.fill();
+});
